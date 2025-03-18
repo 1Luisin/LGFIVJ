227f404a3baf9202adfd2eef4570ac8d592ac975
Luis Guilherme <guilherme32698@gmail.com>
2025-03-17 21:31:02 -0300

    tratamento excessões

---
+++ Pessoal/TratamentoExcessao.Pessoal/TratamentoExcessao.Pessoal/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TratamentoExcessao.Pessoal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int numerador = 10;
+                int denominador = 0;
+                int resultado = numerador / denominador;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Erro: Divisão por zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Fim do programa.");
+                Console.ReadLine();
+            }
+
+            
+        }
+
+
+    }
+}
