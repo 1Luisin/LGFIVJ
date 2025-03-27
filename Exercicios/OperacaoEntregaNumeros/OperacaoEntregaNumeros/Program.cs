70fed1d2e6b6557a2468557806af08c451a66985
Luis Guilherme <guilherme32698@gmail.com>
2025-03-26 21:50:11 -0300

    mais exercicios

---
+++ Exercicios/OperacaoEntregaNumeros/OperacaoEntregaNumeros/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace OperacaoEntregaNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            int primeironumero;
+
+            Console.WriteLine("Informe o primeiro numero");
+
+            char simbolooperacao;
+
+            Console.WriteLine("Informe o tipo de operação: +, -, * ou /");
+
+            int segundonumero;
+
+            Console.WriteLine("Informe o segundo número");
+            
+            segundonumero = Convert.ToInt32(Console.ReadLine());
+            
+            primeironumero = Convert.ToInt32(Console.ReadLine());
+            
+            
+            
+            simbolooperacao = Convert.ToChar(Console.ReadLine());
+
+            int operacao = primeironumero + simbolooperacao + segundonumero;
+            
+            
+        }
+    }
+}
