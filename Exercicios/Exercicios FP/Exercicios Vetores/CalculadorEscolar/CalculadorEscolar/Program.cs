b4df68565b5f5596fb5eab56f34b5ad1e6bb0dd0
Luis Guilherme <guilherme32698@gmail.com>
2025-05-23 21:34:10 -0300

    Projeto Calculadora de média por vetores

---
+++ Exercicios/Exercicios FP/Exercicios Vetores/CalculadorEscolar/CalculadorEscolar/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculadorEscolar
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int contador;
+
+            Console.WriteLine("Bem vindo ao Calculador Escolar! \nDigite o número de alunos que você deseja calcular a nota.");
+            int TotalAluno = Convert.ToInt32(Console.ReadLine());
+            int[] NotaAluno = new int[TotalAluno];
+            string[] Nome = new string[TotalAluno];
+            
+            for (contador = 0; contador < TotalAluno; contador++)
+            {   
+                Console.WriteLine("Digite o nome do aluno {0} de {1}", contador + 1, TotalAluno);
+                Nome[contador] = Console.ReadLine();
+                Console.WriteLine("Digite a nota do(a) " + Nome[contador]);
+                NotaAluno[contador] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+    }
+}
