ecabb16e11f970bf2627f7d6c2f9fc912b8a5589
Luis Guilherme <guilherme32698@gmail.com>
2025-05-27 10:51:25 -0300

    POO em c#

---
+++ Fundamentos programação/Poo/Poo/Poo/Filme.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo
+{
+    internal class Filme
+    {
+        public string nome;
+        public string tipo;
+        public string data;
+        public string estudio;
+
+
+        public void Executar() 
+        {
+
+            Console.WriteLine("Rodando Filme: {0}", nome);
+            Console.WriteLine("Data do Filme: {0}", data);
+            Console.WriteLine("Estúdio do Filme: {0}", estudio);
+        
+        }
+    
+        public void Pausar() 
+        {
+        
+        
+        }
+    }
+}
