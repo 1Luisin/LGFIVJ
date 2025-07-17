89555ac780fa76d85b09b3c4bb245c5e0a2e89c5
Luis Guilherme <guilherme32698@gmail.com>
2025-07-17 14:24:58 -0300

    action.js

---
+++ Programação pessoal/Javascript/action.js
@@ -0,0 +1,15 @@
+// configuração de variaveis 
+var numero = 123; // variavel global
+let numero = 123; // variavel local
+const numero = 123; // variavel estática
+
+// tipagem da linguagem
+
+String;
+Number;
+CharacterData;
+
+console.log(window.numero) // window 
+
+console.log(nome);
+var nome = "Julia";
