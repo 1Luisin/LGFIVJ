4ad85cb4f332c303db922abde600a506b05de13a
Luis Guilherme <guilherme32698@gmail.com>
2025-07-25 15:58:43 -0300

    testando arraylist com numeros

--- Programação pessoal/Java/ArrayList/ArrayListCars/src/App.java
+++ Programação pessoal/Java/ArrayList/ArrayListCars/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         ArrayList<String> cars = new ArrayList<String>();
-        cars.add("Volvo" + "BMW");
+        cars.add("Volvo " + " BMW");
         cars.add ("Toyota");
         cars.add("volkswagen");
         cars.add("Ford");
