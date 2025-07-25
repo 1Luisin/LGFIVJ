a4f9624a7f0f49ded985ad294d627630a426d52a
Luis Guilherme <guilherme32698@gmail.com>
2025-07-25 10:59:45 -0300

    importando biblioteca Scanner

--- Programação pessoal/Java/Exercicios/NumerosPrimos/src/App.java
+++ Programação pessoal/Java/Exercicios/NumerosPrimos/src/App.java
@@ -1,5 +1,8 @@
 public class App {
+    //Verificador de números inteiros
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo ao programa\nDigite um número inteiro ");
+
+
     }
 }
