8beb01509034d14ee9749455cbe3d7ca249376fe
Luis Guilherme <guilherme32698@gmail.com>
2025-07-25 11:21:09 -0300

    testando condições

--- Programação pessoal/Java/Exercicios/NumerosPrimos/src/App.java
+++ Programação pessoal/Java/Exercicios/NumerosPrimos/src/App.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     //Verificador de números inteiros
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem vindo ao programa\nDigite um número inteiro ");
+        
+        int options;
+        
+        System.out.println("Bem vindo ao programa\n1 - Começar\n2 - Sair");
+        Scanner leitura = new Scanner(System.in);
+        
+        options = leitura.nextInt();
+        
+        switch(options){
+                case 1:
+                System.out.println("Digite qualquer número!");
+                int EntradaUsuario;
+                EntradaUsuario = leitura.nextInt();
+                    if ((EntradaUsuario / 2) != 0){
+                        System.out.println("Número x");
+                    }    
 
+            }
+        
 
     }
 }
