842785ed465787928deb66eef2232976f061bb89
Luis Guilherme <guilherme32698@gmail.com>
2025-07-25 15:55:17 -0300

    treino com arrayList

---
+++ Programação pessoal/Java/ArrayList/ArrayListCars/src/App.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo" + "BMW");
+        cars.add ("Toyota");
+        cars.add("volkswagen");
+        cars.add("Ford");
+
+        System.out.println(cars);
+
+
+
+
+    }
+}
