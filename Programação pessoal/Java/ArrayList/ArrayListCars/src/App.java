82566589764f438c44bc71ada3c55f3ede0b5fbb
Luis Guilherme <guilherme32698@gmail.com>
2025-07-25 16:06:02 -0300

    testando outros tipos com arraylist

--- Programação pessoal/Java/ArrayList/ArrayListCars/src/App.java
+++ Programação pessoal/Java/ArrayList/ArrayListCars/src/App.java
@@ -11,7 +11,20 @@ public class App {
 
         System.out.println(cars);
 
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
 
+        System.out.println(numeros);
+
+        ArrayList<Double> NumberFloat = new ArrayList<Double>();
+
+        NumberFloat.add(3.14);
+        NumberFloat.add(120.3);
+
+        System.out.println(NumberFloat);
 
 
     }
