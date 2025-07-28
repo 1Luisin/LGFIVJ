3cef265ccaead5c1d6b3b24df795dff915771438
Luis Guilherme <guilherme32698@gmail.com>
2025-07-28 10:42:36 -0300

    trazsendo a classe pessoa pro main

--- Programação pessoal/Java/modificando_acesso/TreinoModificadorDeAcesso/src/App.java
+++ Programação pessoal/Java/modificando_acesso/TreinoModificadorDeAcesso/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner Leitura = new Scanner(System.in);        
         
+        Pessoa Pessoaobj = new Pessoa();
+        System.out.println("Digite uma idade");
+        Pessoaobj.idade = Leitura.nextInt();
+        
+        System.out.println("Digite um nome");
+        Pessoaobj.nome= Leitura.next();
+
+        
+    
     }
 }
--- Programação pessoal/Java/modificando_acesso/TreinoModificadorDeAcesso/src/pessoa.java
+++ Programação pessoal/Java/modificando_acesso/TreinoModificadorDeAcesso/src/pessoa.java
@@ -1,4 +1,13 @@
-class pessoa {
+public class Pessoa {
 
+    public String nome;
+    public int idade;
 
+    public void ExibirConsole(){
+
+        System.out.println("Informações da pessoa:\n");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    
+    }
 }
