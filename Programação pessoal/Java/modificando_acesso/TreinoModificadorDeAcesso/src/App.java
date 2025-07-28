b9693bfd7c6cbde826239185f72f339f63116cbc
Luis Guilherme <guilherme32698@gmail.com>
2025-07-28 12:55:26 -0300

    adicionando loop de repetição no menu

--- Programação pessoal/Java/modificando_acesso/TreinoModificadorDeAcesso/src/App.java
+++ Programação pessoal/Java/modificando_acesso/TreinoModificadorDeAcesso/src/App.java
@@ -3,11 +3,31 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {        
         
+        Scanner Leitura = new Scanner(System.in);
+
+        boolean sair = true;
+        while(sair != true){
         Pessoa Pessoaobj = new Pessoa();
         System.out.println("Cadastro de usuário!\n");
-        
-        Pessoaobj.RecebeDados();
-        Pessoaobj.RetornaDados();
+            System.out.println("1 - Começar\n2 - Sair");
+                
+                int opcaoUsuario = Leitura.nextInt();
+                    switch (opcaoUsuario) {
+                        case 1:
+                            
+                      Pessoaobj.RecebeDados();
+                      Pessoaobj.RetornaDados();
+                     
+                      break;
+                         
+                      case 2:
+                        sair = true;
+                        
+                        default:
+                            System.out.println("Comando não encontrado!");
+                            break;
+                    }
+            }  
         
     }
 }
