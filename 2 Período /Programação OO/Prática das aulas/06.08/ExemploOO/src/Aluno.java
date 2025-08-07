b2b19eacf0a4616aa515b678a5e02de82c639bca
Luis Guilherme <guilherme32698@gmail.com>
2025-08-06 21:18:54 -0300

    Testando o BlueJ

---
+++ 2 Período /Programação OO/Prática das aulas/06.08/ExemploOO/src/Aluno.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Aluno {
+    
+    Scanner Leitura = new Scanner(System.in);
+    public int Nota;
+    public String Nome;
+    
+    static void CondicaoNota(){
+          Nota = Leitura.nextint();
+        
+        
+    }
+}
