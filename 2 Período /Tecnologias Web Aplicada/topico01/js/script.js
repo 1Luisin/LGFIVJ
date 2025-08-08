80dee1251b95b3e2a10921dd53eb076949b8cfe8
Luis Guilherme <guilherme32698@gmail.com>
2025-08-07 21:56:58 -0300

    aula 01 javascript finalização

--- 2 Período /Tecnologias Web Aplicada/topico01/js/script.js
+++ 2 Período /Tecnologias Web Aplicada/topico01/js/script.js
@@ -4,4 +4,6 @@ console.log(nome);
 var int = 32;
 console.log(int);
 
+nome2 = "awfwef";
+console.log(nome2);
 
--- 2 Período /Tecnologias Web Aplicada/topico01/js/script2.js
+++ 2 Período /Tecnologias Web Aplicada/topico01/js/script2.js
@@ -4,3 +4,8 @@ const matricula_aluno = 32884;
 console.log(aluno);
 console.log(idade_aluno);
 console.log(matricula_aluno);
+
+var nome3;
+console.log(nome3) //undefined
+
+// 
